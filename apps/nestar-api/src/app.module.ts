1dd08407d8e8bfb4a60b632a69622a1ba3df625f
sanjar <sanjarbekkarimov229@gmail.com>
2026-08-19 01:51:28 +0900

    feat: modify nest cli detail

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -10,15 +10,15 @@ import { DatabaseModule } from './database/database.module';
 
 @Module({
 	imports: [
-		ConfigModule.forRoot(),
+		ConfigModule.forRoot(), // .env
 		GraphQLModule.forRoot({
 			driver: ApolloDriver,
 			playground: true,
 			uploads: false,
 			autoSchemaFile: true,
 		}),
-		ComponentsModule,
-		DatabaseModule,
+		ComponentsModule, // Modullar koprigi
+		DatabaseModule, // Database TCP
 	],
 	controllers: [AppController],
 	providers: [AppService, AppResolver],
