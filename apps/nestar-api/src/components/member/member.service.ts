357f8e2939bec798a796469ef245e5ad32f705a3
sanjar <sanjarbekkarimov229@gmail.com>
2026-09-04 05:17:14 +0900

    fix: modify getProperty and getBoardArticle business logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -93,6 +93,11 @@ export class MemberService {
 				await this.memberModel.findOneAndUpdate(search, { $inc: { memberViews: 1 } }, { new: true }).exec();
 				targetMember.memberViews++;
 			}
+
+			//meliked
+			const likeInput = { memberId: memberId, likeRefId: targetId, likeGroup: LikeGroup.MEMBER };
+			targetMember.meLiked = await this.likeService.checkLikeExistence(likeInput);
+			//mefollowed
 		}
 
 		return targetMember;
